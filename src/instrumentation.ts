bd866b74b08964275ae5eb7232d51d1ba068403c
chenwl <qzchenwl@gmail.com>
2025-10-07 21:43:09 +0800

    feat: sentry remove package.json

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
+import { getCloudflareContext } from '@opennextjs/cloudflare';
 
 export async function register() {
+  const { env } = await getCloudflareContext({ async: true });
+  console.log('instrumentation', env);
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('../sentry.server.config');
   }
