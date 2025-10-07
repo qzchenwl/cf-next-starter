c4fb2fabae4be9cd158bfdcda7e90a3fd397f30b
chenwl <qzchenwl@gmail.com>
2025-10-07 12:00:39 +0800

    feat: send verification email

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,25 @@
-import { betterAuth, type BetterAuthOptions } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { drizzle as drizzleD1 } from "drizzle-orm/d1";
+import { betterAuth, type BetterAuthOptions } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { drizzle as drizzleD1 } from 'drizzle-orm/d1';
 
-import * as authSchema from "@/db/auth-schema"; // 按你的实际路径改
+import * as authSchema from '@/db/auth-schema';
+import { sendVerificationEmail } from '@/lib/email';
 
 export const betterAuthOptions: BetterAuthOptions = {
   emailAndPassword: {
     enabled: true,
   },
-  trustedOrigins: ["http://localhost:8787", "*.workers.dev", "*.cwllll.com"],
+  emailVerification: {
+    sendOnSignUp: true,
+    sendVerificationEmail,
+  },
+  trustedOrigins: ['http://localhost:8787', '*.workers.dev', '*.cwllll.com'],
 };
 
 export async function createAuth(env: CloudflareEnv) {
   return betterAuth({
     database: drizzleAdapter(drizzleD1(env.D1, { schema: authSchema }), {
-      provider: "sqlite",
+      provider: 'sqlite',
       schema: authSchema,
     }),
     ...betterAuthOptions,
