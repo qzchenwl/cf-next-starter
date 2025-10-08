be3058ae3ce2a3e18b28537456d98e1f0f34e485
chenwl <qzchenwl@gmail.com>
2025-10-08 13:47:05 +0800

    feat: fix custom-worker 8

--- custom-worker.ts
+++ custom-worker.ts
@@ -35,3 +35,8 @@ sentryEnhancedWorker.fetch = async (request, env, ctx) => {
 };
 
 export default sentryEnhancedWorker;
+
+// The re-export is only required if your app uses the DO Queue and DO Tag Cache
+// See https://opennext.js.org/cloudflare/caching for details
+// @ts-expect-error `.open-next/worker.ts` is generated at build time
+export { DOQueueHandler, DOShardedTagCache } from './.open-next/worker.js';
